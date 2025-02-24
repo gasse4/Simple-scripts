c19eb4c263fbba65245e5673d61a28679ccd6bd0
Gasser <gasserm525@gmail.com>
2025-02-24 22:14:31 +0200

    v2

---
+++ task_tracker.py
@@ -0,0 +1,10 @@
+# print a  salute message 
+# make a list off the features like: add,list,update etc...
+# make an addition func to add tasks
+# when adding a task print in green: Task added successfully (ID: 1)
+# when choosing (list) --> prinnting the ID's and the added tasks and the date and time of addition,and the status
+# when choosing update --> updating the indexed item at the list of tasks
+# printing in green that it had been updated with adding the ID
+# when choosing delete --> remove the task
+# when choosing mark done or exit --> exit the programme and print goodbye!
+# using lambda
